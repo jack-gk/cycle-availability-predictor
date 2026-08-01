e11dfccc396815b34227540c56f71ace9d48c917
Jack Graham-Kevan <jaackgk@outlook.com>
2026-08-01 16:56:03 +0100

    Added parquet saving utility, new tables for both observations and station data

--- bronze_to_silver.py
+++ bronze_to_silver.py
@@ -1,7 +1,7 @@
 import json
 import pandas as pd
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 
 logging.basicConfig(
@@ -10,130 +10,173 @@ logging.basicConfig(
     force=True,
 )
 
-bronze_path = Path.cwd()/"data"/"bronze"
+BRONZE_PATH = Path.cwd()/"data"/"bronze"
+SILVER_PATH = Path.cwd() / "data" / "silver"
+OBSERVATIONS_PATH = SILVER_PATH / "observations"
+STATIONS_PATH = SILVER_PATH / "stations"
 
 WANTED_COLUMNS = [
     "NbBikes",
     "NbEBikes",
     "NbStandardBikes",
     "NbDocks",
-    "NbEmptyDocks",
+    "NbEmptyDocks"
+]
+
+REFERENCE_COLUMNS = [
+     "id",
+    "lat",
+    "lon",
+    "commonName"
 ]
 
 REQUIRED_COLUMNS = [
     "id",
-    "additionalProperties",
+    "additionalProperties"
 ]
 
-def validate_file(file: Path) -> dict | None:
+def checkFile(file: Path):
+
+    def fail(name: str, reason: str) -> tuple[pd.DataFrame | None, datetime | None, dict[str, str] | None]:
+            logging.warning("Skipping: %s (%s)", name, reason)
+            return(None, None, {"file": name, "reason": reason})
 
     if not file.is_file():
-        logging.warning("Skipping: %s", file.name)
-        return({"file": file.name, "reason": "Not a file"})
-            
+            return fail(file.name, "Not a file")
+                
     if file.stat().st_size == 0:
-        logging.warning("Skipping empty file: %s", file.name)
-        return({"file": file.name, "reason": "Empty File"})
+        return fail(file.name, "Empty file")
 
     if file.name.endswith("_FAILED.json"):
-        logging.warning("Skipping failed file: %s", file.name)
-        return({"file": file.name, "reason": "Failed API Request"})
-        
-        
-    valid_name = (
-        file.name.startswith("tfl-bikes-")
-        and file.name.endswith(".json")
-        and len(file.name) == 33
-    )
+        return fail(file.name, "Failed API request")
 
-    if not valid_name:
-        logging.error("Invalid filename: %s", file.name)
-        return {
-            "file": file.name,
-            "reason": "Invalid naming convention",
-        }
-        
-    return None
+    if len(file.name) != 33:
+        return fail(file.name, "Invalid filename size")
 
-def transform_snapshot(file: Path) -> pd.DataFrame | dict:
-            
-        flag = validate_file(file)
-        
-        if flag is not None:
-            return None, flag
-        try:
-            time = (file.name[10:-5])
-            date_time = datetime.strptime(
-                time,
-                "%Y-%m-%d-H%H-M%M"
-            )
-
-            with file.open("r", encoding="utf-8") as f:
-                json_input = json.load(f)
-    
-            if not isinstance(json_input, list):
-                logging.warning(
-                    "%s contains %s instead of a list",
-                    file.name,
-                    type(json_input).__name__,
-                )
-                return(None, {"file":file.name, "reason":"Not JSON input"})
-
-            if not json_input:
-                logging.warning("%s contains an empty list", file.name)
-                return(None, {"file":file.name, "reason":"Contains an empty list"})
-
-        
-        except ValueError as e:
-            logging.warning(f"{file.name} failed to extract datetime")
-            return(None, {"file":file.name, "reason":"Couldn't Extract Datetime"})
-        
-        except json.JSONDecodeError as e:
-            logging.warning(f"Skipping invalid JSON file: {file.name}")
-            return(None, {"file":file.name, "reason":"Invalid JSON input"})
-
-        except Exception as e:
-            logging.warning(f"Unexpected error in file: {file.name}")
-            return(None, {"file":file.name, "reason":e})
-
-        
-        df = pd.json_normalize(json_input)
-    
-        df = df[REQUIRED_COLUMNS]
-        df["date_time"] = date_time
-
-        df_long = df.explode("additionalProperties",True)
-
-        properties = pd.json_normalize(
-            df_long["additionalProperties"]
+    try:
+        time = (file.name[10:-5])
+        date_time = datetime.strptime(
+            time,
+            "%Y-%m-%d-H%H-M%M"
         )
-        dfx = pd.concat(
-            [
-                df_long[["id", "date_time"]],
-                properties
-            ],
-            axis=1
-        ) 
+
+        with file.open("r", encoding="utf-8") as f:
+            json_input = json.load(f)
+
+        if not isinstance(json_input, list):
+            return fail(file.name, "Not JSON input")
+
+        if not json_input:
+            return fail(file.name, "Contains an empty list")
         
-        dfx_filtered = dfx[["id", "date_time", "key", "value"]]
-        wanted = ["NbBikes", "NbStandardBikes", "NbEBikes", "NbEmptyDocks", "NbDocks"]
+    except json.JSONDecodeError as e:
+        return fail(file.name, "Invalid JSON input")
 
-        wanted_rows = dfx_filtered[dfx_filtered["key"].isin(wanted)]
+    except OSError as error:
+        return fail(file.name, f"Could not read file: {error}")
+    
+    except ValueError as e:
+        return fail(file.name, "Couldn't extract datetime")
 
-        df_pivot = wanted_rows.pivot(index=["id", "date_time"], columns="key", values="value")
+    except Exception as e:
+        return fail(file.name, f"Unexpected error: {e}")
 
-        new_df = df_pivot.loc[:, WANTED_COLUMNS].copy()
-        new_df = new_df.astype("Int64")
-        new_df = new_df.reset_index()
+    return pd.json_normalize(json_input), date_time, None
 
-        return new_df, None
+def transform_snapshot(file: Path) -> tuple[pd.DataFrame | None, dict[str, str] | None]:
+
+    raw_df, date_time, error = checkFile(file)
+
+    if error is not None:
+         return None, error
+
+    missing_columns = []
+
+    for column in REFERENCE_COLUMNS:
+        if column not in raw_df.columns:
+            missing_columns.append(column)
+
+    if missing_columns:
+        return None, {"file":file.name, "reason":f"Missing column(s): {missing_columns}"}
+
+    df = raw_df[REQUIRED_COLUMNS]
+    
+    df["date_time"] = date_time
+
+    df_long = df.explode("additionalProperties",True)
+
+    properties = pd.json_normalize(
+        df_long["additionalProperties"]
+    )
+    dfx = pd.concat(
+        [
+            df_long[["id", "date_time"]],
+            properties
+        ],
+        axis=1
+    ) 
+    
+    dfx_filtered = dfx[["id", "date_time", "key", "value"]]
+
+    wanted_rows = dfx_filtered[dfx_filtered["key"].isin(WANTED_COLUMNS)]
+
+    df_pivot = wanted_rows.pivot(index=["id", "date_time"], columns="key", values="value")
+
+    new_df = df_pivot.loc[:, WANTED_COLUMNS].copy()
+    new_df = new_df.astype("Int64")
+    new_df = new_df.reset_index()
+
+    return new_df, None
+
+def create_station_ref(file: Path) -> tuple[pd.DataFrame | None, dict[str, str] | None]:
+
+    raw_df, date_time, error = checkFile(file)
+
+    if error is not None:
+         return None, error
+
+    missing_columns = []
+
+    for column in REFERENCE_COLUMNS:
+        if column not in raw_df.columns:
+            missing_columns.append(column)
+    
+    if missing_columns:
+        return None, {"file":file.name, "reason":f"Missing column(s): {missing_columns}"}
+
+    station_df = raw_df[REFERENCE_COLUMNS].copy()
+
+    station_df = station_df.drop_duplicates(subset=["id"], keep="last").sort_values("id").reset_index(drop=True)
+
+    station_df["updated_at"] = date_time
+
+    return station_df, None
+
+def save_station_ref(station_df: pd.DataFrame) -> Path:
+    STATIONS_PATH.mkdir(parents=True, exist_ok=True)
+    output_file = STATIONS_PATH/"stations.parquet"
+
+    station_df.to_parquet(output_file, index=False)
+    logging.info(f"Saved {len(station_df)} stations to {output_file}")
+
+    return output_file
+
+def save_daily_observation(daily_df: pd.DataFrame, day: str) -> Path:
+    day_path = OBSERVATIONS_PATH / f"date={day}"
+    day_path.mkdir(parents=True, exist_ok=True)
+    output_file = day_path/"observation.parquet"
+
+    daily_df.to_parquet(output_file, index=False)
+    logging.info(f"Saved {len(daily_df)} observations to {output_file}")
+
+    return output_file
 
 def transform_day(day: str) -> pd.DataFrame | None:
-    snapshot_dfs = []
+    observation_dfs = []
     failed_files = []
-    
+
     day_files = sorted(
-        bronze_path.glob(f"tfl-bikes-{day}*.json")
+        BRONZE_PATH.glob(f"tfl-bikes-{day}*.json")
     )
 
     for file in day_files:
@@ -141,24 +184,24 @@ def transform_day(day: str) -> pd.DataFrame | None:
         if not file.name.startswith(f"tfl-bikes-{day}"):
             continue
 
-        snapshot_df, error = transform_snapshot(file)
+        observation_df, error = transform_snapshot(file)
 
         if error is not None:
             failed_files.append(error)
             continue
         
-        snapshot_dfs.append(snapshot_df)
+        observation_dfs.append(observation_df)
         logging.info(file.name)
 
-    if not snapshot_dfs:
+    if not observation_dfs:
             return None, failed_files
 
     daily_df = pd.concat(
-        snapshot_dfs,
+        observation_dfs,
         ignore_index=True,
     )
 
-    successful_count = len(snapshot_dfs)
+    successful_count = len(observation_dfs)
     failed_count = len(failed_files)
     total_count = successful_count + failed_count
 
@@ -172,17 +215,69 @@ def transform_day(day: str) -> pd.DataFrame | None:
 
     return daily_df, failed_files
 
+#mode = "instance"
+#mode = "day"
+#mode = "ref"
+mode = "catchup"
 
 def main() -> None:
-    day_string = "2026-07-24"
 
-    daily_df, failed_files = transform_day(day_string)
+    def findStart() -> datetime:
+        for file in BRONZE_PATH.iterdir():
+            try:
+                date_text = file.name[10:20]
+                parsed_date = datetime.strptime( date_text, "%Y-%m-%d").date()
+            except Exception as e:
+                logging.warning("Error:",e)
+        
 
-    if daily_df is not None:
-        print(daily_df.tail(5))
+    if mode == "catchup":
+        logging.info("Catch Up Loading...")
+        start_day = datetime(2026, 8, 24)
+        end_day = datetime(2026, 8, 29)
 
-    for error in failed_files:
-        print(error)
+        while start_day <= end_day:
+            day_string = start_day.strftime("%Y-%m-%d")
+
+            logging.info("Processing catch-up date: %s", day_string)
+
+            daily_df, failed_files = transform_day(day_string)
+
+            if daily_df is not None:
+                save_daily_observation(
+                    daily_df,
+                    day_string,
+                )
+            else:
+                logging.warning(f"No file for date {day_string}")
+
+            start_day = start_day + timedelta(1)
+
+
+    if mode == "day":
+        day_string = "2026-07-25"
+        daily_df, failed_files = transform_day(day_string)
+        if daily_df is not None:
+            print(daily_df.tail(5))
+            save_daily_observation(daily_df, day_string)
+
+        for error in failed_files:
+            print(error)
+
+    else:
+         
+        snapshot = "tfl-bikes-2026-07-24-H12-M45.json"
+        file = BRONZE_PATH / snapshot
+
+        if mode == "instance":
+            df, errors = transform_snapshot(file)
+            print(df.head(10))
+
+        if mode == "ref":
+            df_ref, fail = create_station_ref(file)
+            if fail is None:
+                print(df_ref.head(10))
+                save_station_ref(df_ref)
 
 
 if __name__ == "__main__":
